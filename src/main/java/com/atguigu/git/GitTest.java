304512d7f85382c893fb23fdc176756771d7f34d
csy <2548143425@qq.com>
2022-01-16 13:53:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author csy
+ * @create 2022-01-16 13:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
